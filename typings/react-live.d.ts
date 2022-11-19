17e4bd8fdfa8d0493a8ab250e3c526e0c0e98ba3
Ali Emir Sen <senaliemir@gmail.com>
2022-11-19 15:20:28 +0300

    refactor: add `Context` prop to types

--- typings/react-live.d.ts
+++ typings/react-live.d.ts
@@ -1,37 +1,39 @@
-import { ComponentClass, HTMLProps, ComponentType, Context } from 'react'
-import { PrismTheme, Language } from 'prism-react-renderer';
+import { ComponentClass, HTMLProps, ComponentType, Context } from "react";
+import { PrismTheme, Language } from "prism-react-renderer";
 
 // Helper types
-type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>
+type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
 
 // React Element Props
-type DivProps = HTMLProps<HTMLDivElement>
-type PreProps = HTMLProps<HTMLPreElement>
+type DivProps = HTMLProps<HTMLDivElement>;
+type PreProps = HTMLProps<HTMLPreElement>;
 
 // LiveProvider
-export type LiveProviderProps = Omit<DivProps, 'scope'> & {
+export type LiveProviderProps = Omit<DivProps, "scope"> & {
+  Context?: typeof LiveContext;
   scope?: { [key: string]: any };
   code?: string;
   noInline?: boolean;
-  transformCode?: (code: string) => (string | Promise<string>);
+  transformCode?: (code: string) => string | Promise<string>;
   language?: Language;
   disabled?: boolean;
   theme?: PrismTheme;
-}
+};
 
-export const LiveProvider: ComponentClass<LiveProviderProps>
+export const LiveProvider: ComponentClass<LiveProviderProps>;
 
 // Editor
-export type EditorProps = Omit<PreProps, 'onChange'> & {
-  code?: string,
+export type EditorProps = Omit<PreProps, "onChange"> & {
+  Context?: typeof LiveContext;
+  code?: string;
   disabled?: boolean;
   language?: Language;
   onChange?: (code: string) => void;
   theme?: PrismTheme;
-  prism?: unknown
-}
+  prism?: unknown;
+};
 
-export const Editor: ComponentClass<EditorProps>
+export const Editor: ComponentClass<EditorProps>;
 
 // Context
 export interface ContextProps {
@@ -47,13 +49,15 @@ export const LiveContext: Context<ContextProps>;
 // LiveEditor
 export type LiveEditorProps = EditorProps;
 
-export const LiveEditor: ComponentClass<LiveEditorProps>
+export const LiveEditor: ComponentClass<LiveEditorProps>;
 
 // LiveError
-export const LiveError: ComponentClass<DivProps>
+export const LiveError: ComponentClass<DivProps>;
 
 // LivePreview
-export const LivePreview: ComponentClass<DivProps>
+export const LivePreview: ComponentClass<DivProps>;
 
 // withLive HOC
-export function withLive<P>(wrappedComponent: ComponentType<P>): ComponentClass<P>
+export function withLive<P>(
+  wrappedComponent: ComponentType<P>
+): ComponentClass<P>;
